02d9239dad4ff57a5d0c5511b2a21f5af70d162b
xiaoxionganna <347919507@qq.com>
2018-04-20 16:12:42 +0800

    20180420
    练习

---
+++ FoodManagement/src/Test20180420/Food.java
@@ -0,0 +1,12 @@
+package Test20180420;
+
+class Food {
+	private String no;
+	private String name;
+	private float price;
+	Food(String no,String name,float price){
+		this.no=no;
+		this.name=name;
+		this.price=price;
+	}
+}
